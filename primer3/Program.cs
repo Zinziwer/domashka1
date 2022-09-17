a4f1db428b15022175b76e20e6b5444ca49e49be
Zinziwer <unislon@mail.ru>
2022-09-17 14:56:25 +0300

    even/odd

---
+++ primer3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("enter the number ");
+int num = Convert.ToInt32 (Console.ReadLine());
+if(num % 2 == 0)
+{
+    Console.Write("Your number " + num +" is an even number");
+}
+else
+{
+    Console.Write("Your number " + num +" is an odd number");
+}
