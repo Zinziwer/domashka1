91985871f0ecee366a547eeb635c16281f3bb3b2
Zinziwer <unislon@mail.ru>
2022-09-17 05:08:56 +0300

    max of three

---
+++ primer2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("enter first number ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("enter second number ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("enter third number ");
+int num3 = Convert.ToInt32 (Console.ReadLine());
+int maxnum=num1;
+if (maxnum>num2)
+{
+    if(maxnum<num3)
+    {
+        maxnum=num3;
+    }
+}
+else 
+{   
+    maxnum=num2;
+    if (maxnum<num3)
+    {
+        maxnum=num3;
+    }
+}
+Console.Write("max number "+maxnum);
