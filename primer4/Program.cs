6c330dac73a7648e701bc7567f80d6b7ca11860c
Zinziwer <unislon@mail.ru>
2022-09-17 15:28:40 +0300

    0-N even nunbes

---
+++ primer4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("enter the number ");
+int N = Convert.ToInt32 (Console.ReadLine());
+int tmp = 2;
+while (tmp<=N-2)
+{
+    Console.Write(tmp +", ");
+    tmp=tmp + 2;
+}
+if ( N % 2==0)                  //это я так запятую в конце удаляю
+{
+    Console.Write(N);
+}
+else
+{
+        Console.Write(N-1);
+}
