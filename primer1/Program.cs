096d25be569ac8f49a47cb890d900d5d7eefe5e2
Zinziwer <unislon@mail.ru>
2022-09-17 04:27:42 +0300

    > < question

---
+++ primer1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("enter first number ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("enter second number ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+if (num1>num2)
+{
+    Console.Write("first number "+ num1 + " > second number " + num2);
+}
+else
+{
+    Console.Write("second number "+ num2+ " > first number " + num1 );
+}
